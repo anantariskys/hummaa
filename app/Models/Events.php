3f16058283148840ad7a5a03d8c4bcd2ab852430
anantariskys <gamersindo555@gmail.com>
2025-11-08 10:05:36 +0700

    feat(Models): add Events model with fillable fields and casts

---
+++ app/Models/Events.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Events extends Model
+{
+    use HasFactory;
+
+       protected $fillable = [
+        'title',
+        'subtitle',
+        'description',
+        'badge',
+        'duration',
+        'question_count',
+        'test_parts',
+    ];
+
+    protected $casts = [
+        'test_parts' => 'array',
+    ];
+}
